be3e9cb49afc470a08d05d3bebda9d4245a7091c
grzehooW <grzehoo.Wrzosek@gmail.com>
2022-09-26 13:04:58 +0100

    Predicate

---
+++ src/TestPredicate.java
@@ -0,0 +1,47 @@
+import java.util.function.Predicate;
+
+public class TestPredicate {
+
+    public static void main(String[] args) {
+
+        // Evaluate<T> an Interface
+
+        Evaluate<Integer> lambda = i -> i < 0;
+        System.out.println(lambda.isNegative(-1));
+        System.out.println(lambda.isNegative(1));
+
+        System.out.println();
+
+        // just Regular Predicate<Integer>
+
+        Predicate<Integer> predicate = i -> i > 0;
+        System.out.println(predicate.test(-1));
+        System.out.println(predicate.test(-1));
+
+        System.out.println();
+
+        // Boolean methode check() with two args, Type Integer and Predicate<Integer>
+
+        int x = 4;
+        System.out.println("Is " + x + " even? " + check(x, n -> n % 2 ==0));
+
+        x = 7;
+        System.out.println("Is " + x + " even? " + check(x, n -> n % 2 ==0));
+
+        System.out.println();
+
+        String name = "Mr. joe Bloggs";
+        System.out.println("Does " + name + " start with Mr. ? " + check(name, n -> n.startsWith("Mr.")));
+
+        name = "Ms. Anne Bloggs";
+        System.out.println("Does " + name + " start with Mr. ? " + check(name, n -> n.startsWith("Mr.")));
+
+    }
+
+    public static <T> boolean check(T t, Predicate<T> lambda){
+        return lambda.test(t);
+    }
+}
+ interface Evaluate<T>{
+    boolean isNegative(T t);
+ }
